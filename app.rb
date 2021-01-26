ca6663bb57fbecb0a73aa7e1b4472fc0ef63c397
Stephen <stephenjsweets@gmail.com>
2021-01-26 17:34:37 -0500

    commit tree 9b3269e993636616f4f571e9ebc6f7734e0daa00

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship1 = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+      binding.pry
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+    attr_reader :name, :weight, :height
+
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
+
 end
